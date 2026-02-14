97923bdb45e3f95dd88fd941f1fa21e7dc1bab44
JVliyanarachchi21 <jvliyanaarachchi21@gmail.com>
2026-02-15 01:01:12 +0530

    Initial project setup: Spring Boot + PostgreSQL + Thymeleaf

---
+++ uniintern/src/main/java/com/uniintern/portal/UniInternPortalApplication.java
@@ -0,0 +1,13 @@
+package com.uniintern.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniInternPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniInternPortalApplication.class, args);
+	}
+
+}
---
+++ uniintern/src/test/java/com/uniintern/portal/UniInternPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniintern.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniInternPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
